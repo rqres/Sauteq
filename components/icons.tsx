8d5d8fd8de12fa2a2a4df657acbd84b5e2b1cdae
Rares Tamasanu <rares.ioan@icloud.com>
2023-08-21 18:35:11 +0300

    fix: missing lunch icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -71,6 +71,26 @@ export const Icons = {
       ></path>
     </svg>
   ),
+  lunch: (props: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-sandwich"
+      {...props}
+    >
+      <path d="M3 11v3a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-3" />
+      <path d="M12 19H4a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-3.83" />
+      <path d="m3 11 7.77-6.04a2 2 0 0 1 2.46 0L21 11H3Z" />
+      <path d="M12.97 19.77 7 15h12.5l-3.75 4.5a2 2 0 0 1-2.78.27Z" />
+    </svg>
+  ),
   back: (props: LucideProps) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -83,6 +103,7 @@ export const Icons = {
       strokeLinecap="round"
       strokeLinejoin="round"
       className="lucide lucide-move-left"
+      {...props}
     >
       <path d="M6 8L2 12L6 16" />
       <path d="M2 12H22" />
